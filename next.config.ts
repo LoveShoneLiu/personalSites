38a4f9f020365f3262a2b7fada8448bcd6bc532a
shaofeiliu <2397667718@qq.com>
2025-12-01 15:58:59 +0800

    feat: 在 Vercel 上关闭 sharp 依赖，避免构建失败

--- next.config.ts
+++ next.config.ts
@@ -4,6 +4,11 @@ const nextConfig: NextConfig = {
   compiler: {
     removeConsole: false,
   },
+  images: {
+    // 项目是个人站点，对极致图片优化要求不高，
+    // 在 Vercel 上关闭 sharp 依赖，避免构建失败
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
